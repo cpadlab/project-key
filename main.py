d6842ae68f715bfaf200f5764e90c8a1773be88d
cpadlab <cpadlab@gmail.com>
2026-02-20 12:33:05 +0100

    fix: Ensure logger updates level after config load

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import logging
 import argparse
 from pathlib import Path
-from app.utils.logger import logger
 
+from app.utils.logger import logger
 from app.utils.cli import get_args
 from app.core.config import settings
 
@@ -11,6 +12,11 @@ def main(arguments: argparse.Namespace):
     """
     settings.load_from_ini(Path(arguments.config_file))
     settings.setup_with_args(arguments)
+    
+    new_logging_level = getattr(logging, settings.LOG_LEVEL.upper(), logging.INFO)
+    logger.setLevel(new_logging_level)
+    for handler in logger.handlers:
+        handler.setLevel(new_logging_level)
 
     logger.info(f"--- {settings.PROJECT_NAME} v{settings.VERSION} initialized ---")
     logger.debug(f"Configuration loaded from: {arguments.config_file}")
