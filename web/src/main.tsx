d8d66ba034a3f6d12348cdedca34d9da0e733b6d
cpadlab <cpadlab@gmail.com>
2026-02-22 10:04:49 +0100

    feat: Add Sonner toast provider for UI notifications

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@/App.tsx'
 import '@/styles/global.css'
+import { Toaster } from '@/components/ui/sonner'
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
+        <Toaster />
     </StrictMode>,
 )
