5a374748bcb8fe0355c7060b3274b6ac0e03c7dd
cpadlab <cpadlab@gmail.com>
2026-02-22 10:42:45 +0100

    feat: Update TS definitions for vault and file API

--- web/src/global.d.ts
+++ web/src/global.d.ts
@@ -5,6 +5,11 @@ declare global {
             api: {
                 get_app_version: () => Promise<string>;
                 get_app_name: () => Promise<string>;
+                get_history: () => Promise<{raw: string, display: string}[]>;
+                clear_history: () => Promise<boolean>;
+                save_file_dialog: (filename: string, ext: string) => Promise<string | null>;
+                create_vault: (path: string, password: string, keyfile: string | null) => Promise<boolean>;
+                generate_keyfile: (path: string) => Promise<string | null>;
             }
         }
     }
